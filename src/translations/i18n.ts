84dff27e7f7ef7aaeb47953e25817869e99998a3
Jonathan Laabs <jonathan.laabs@gmail.com>
2025-08-23 12:26:03 +0200

    adds proper language detection based on user preferences

--- src/translations/i18n.ts
+++ src/translations/i18n.ts
@@ -1,8 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { getLocales } from 'expo-localization';
 
 import * as de from '@/src/translations/de.json';
 import * as en from '@/src/translations/en.json';
+
+const locales = getLocales();
 
 const resources = {
   de: {
@@ -13,12 +16,18 @@ const resources = {
   },
 };
 
+const supportedLanguages = Object.keys(resources);
+
+const language =
+  locales.find((locale) => locale.languageCode !== null && supportedLanguages.includes(locale.languageCode))
+    ?.languageCode ?? undefined;
+
 // eslint-disable-next-line import/no-named-as-default-member
 i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'en',
+    lng: language,
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false,
